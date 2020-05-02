cab894cabd72baa7913337656eaa1cefc9bf4732
SOLOMON YISMAW <fantahunsolomon@gmail.com>
2020-05-02 00:06:49 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## NYC Pigeon Organizer
+  Ken## NYC Pigeon Organizer
 
 ## Learning Goals
 
--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+      
+  nyc_pigeon_sorted = Hash.new
+  
+  data.each do |color, color_collection|
+    color_collection.each do |color_type, name_collection|
+      name_collection.each do |name|
+    if !nyc_pigeon_sorted [name]
+  data.each do |gender, m_or_f|
+    
+    #binding.pry
+  
+  nyc_pigeon_sorted [name] = { }
+  
+    end
+
+end
+end
+    
+end
+nyc_pigeon_sorted
 end
